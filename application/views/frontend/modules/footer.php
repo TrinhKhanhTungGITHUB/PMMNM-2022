8be18815298f1b2f80e554fe241381837b92fe31
TrinhKhanhTungGITHUB <tungdrivehaui01@gmail.com>
2022-12-06 21:25:46 +0700

    update

---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,60 @@
+<footer id="footer">
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+          </div>
+      </div>
+  </div>
+  <div class="top-footer">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <div class="col-xs-12 col-sm-6">
+                <a href="trang-chu">
+                    <img src="public/images/smart-mobi2.png" style="width: 100%;" class="img-fix f-logo" alt="smart-construction">
+                </a>
+                <div class="about-store">
+                 Shop Smart phone. Chất lượng - giá cả hợp lý
+             </div>
+         </div>
+         <div class="col-xs-12 col-sm-6">
+            <div class="f-contact">
+                <h3>Thông tin liên hệ</h3>
+                <ul class="list-unstyled">
+                    <li class="clearfix">
+                        <i class="fa fa-map-marker"></i>
+                        <span>298 Cầu Diễn, Bắc Từ Liêm,Hà Nội </span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-phone"></i>
+                        <span>0879.851.172 - 0211.686.868</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-envelope"></i>
+                        <span><a href="mailto:tungdrivehaui01@gmail.com">tungdrivehaui01@gmail.com</a></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="col-xs-12 col-sm-6">
+            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
+            <ul class="list-unstyled linklists">
+                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
+                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
+                <li><a href="dieu-khoan">Điều khoản</a></li>                   
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+        <iframe width="340" height="200" src="https://www.youtube.com/embed/syquiz31_pQ" title="Tình Khúc Nhạc Trẻ 8x9x Chọn Lọc💞Tình lỡ cách xa, tình em là đại dương...💖💖💖" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       </div>
+   </div>
+</div>
+</div>
+</footer>
