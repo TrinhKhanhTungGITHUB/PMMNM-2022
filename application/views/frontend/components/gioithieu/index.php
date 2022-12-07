d9841fe597d0ac57b7e7df027830c0e77bcb09b8
TrinhKhanhTungGITHUB <tungdrivehaui01@gmail.com>
2022-12-08 00:35:50 +0700

    sua vai loi + BaiViet + KhachHang+ Gioi Thieu + GiaoDienQL

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,160 @@
+<section class="cs-services">
+    <div class="container container--medium">
+        <h2 class="cs-services__heading">
+            Câu chuyện mô hình doanh nghiệp CÓ TRÁCH NHIỆM<br>
+            Coolmate đang hướng tới
+        </h2>
+        <div id="services1" class="grid grid--aligned-center grid--two-columns grid--doubling">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #1. Với Khách Hàng
+                </h3>
+                <div class="cs-services__description">
+                    <p>Smart Store kỳ vọng mang lại sản phẩm chất lượng hơn nhờ mô hình quản lý chuỗi cung ứng từ gốc (với
+                        ngành may mặc đó là từ sợi), Smart Store cũng có các đối tác ở quy mô toàn cầu, và chất lượng về
+                        nguyên liệu đạt tiêu chuẩn cao trong ngành may mặc, có thể so sánh với các thương hiệu lớn và
+                        lâu đời. </p>
+                    <p>Ngoài ra mô hình bán hàng trực tiếp (Ecommerce D2C) cho phép Smart Store được đầu tư nhiều hơn cho
+                        phần sản xuất thay vì phải liên tục cắt giảm giá vốn như các mô hình truyền thống! Thực tế khách
+                        hàng sẽ thấy được chất lượng các sản phẩm Smart Store đã cải thiện đáng kể trong năm gần đây và
+                        chắc chắn trong tương lai nữa. </p>
+                    <p>Smart Store kỳ vọng mang lại giá cả tốt hơn cho khách hàng nhờ vào mô hình bán hàng trực tiếp thông
+                        qua nền tảng TMĐT. Thay vì việc phải bán giá sản phẩm x4-x6 lần giá vốn như truyền thống thì
+                        Smart Store thường là x1.8-x2.5 lần mà chúng tôi vẫn có phần lời nhất định. Ngoài ra Smart Store còn
+                        hướng tới mang lại một trải nghiệm mua sắm tốt hơn dành cho khách hàng nhờ vào việc tập trung
+                        rất nhiều vào dịch vụ khách hàng (<a
+                            href="#"
+                            style="text-decoration: underline;">tham khảo 11 cam kết của Smart Store dành cho khách
+                            hàng</a>)</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img src="https://mcdn.coolmate.me/uploads/April2022/Group_1_(1).png"
+                        alt="image"> <span class="cs-services__alt">
+                        Ảnh tại Coolmate Hub
+                    </span></div>
+            </div>
+        </div>
+        <div id="services2" class="grid grid--aligned-center grid--two-columns grid--doubling">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #2. Với Nhân Viên Smart Store
+                </h3>
+                <div class="cs-services__description">
+                    <p>Smart Store kỳ vọng sẽ xây dựng được một tổ chức cởi mở, một môi trường năng động với một văn hóa
+                        riêng (tham khảo văn hóa Smart Store). Và ở đó các bạn trẻ sẽ phát huy hết khả năng của mình, và
+                        nhận được những phần lương, thưởng xứng đáng. Ngoài lương thì các bạn nhân viên của Smart Store còn
+                        nhận được các ESOP (cổ phần thưởng) hàng năm nếu có đóng góp tốt.</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img src="https://mcdn.coolmate.me/uploads/April2022/sn34-5_2.png"
+                        alt="image"> <span class="cs-services__alt">Ảnh các bạn nhân viên HCM vào sinh nhật Smart Store
+                        3</span></div>
+            </div>
+        </div>
+        <div id="services3" class="grid grid--aligned-center grid--two-columns grid--doubling grid--mobile-rev">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #3. Với Đối Tác Smart Store
+                <div class="cs-services__description">
+                    <p>Smart Store làm trong ngày may mặc. Một ngành mà là thế mạnh của Việt Nam, tuy nhiên thì đời sống
+                        của những người công nhân may chưa bao giờ được đảm bảo đủ tốt. Việc giảm chi phí gia công để
+                        tăng sức cạnh tranh cho sản phẩm là chiến lược được các thương hiệu lớn liên tục áp dụng.
+                        Smart Store muốn thay đổi điều đó bằng việc luôn cố gắng trả nhiều hơn cho những người thợ may nếu
+                        có thể (Yêu cầu các đối tác phải đóng bảo hiểm cho người lao động là một ví dụ). </p>
+                    <p>Ngoài ra thì Smart Store cũng có nhiều hoạt động hỗ trợ các bạn công nhân may khác như chăm sóc các
+                        bạn F0, hỗ trợ giai đoạn thiếu việc làm, hay là việc theo đuổi mô hình sản xuất chất lượng, thay
+                        vì giá rẻ. Smart Store kỳ vọng rằng sẽ góp một phần vào việc thay đổi nhận thức của các thương hiệu
+                        về mối quan hệ với các xưởng sản xuất, và đời sống người lao động. Mô hình lao động giá rẻ của
+                        ngành dệt may không phải là mô hình bền vững và có giá trị lâu dài trong tương lai.</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img src="https://mcdn.coolmate.me/uploads/April2022/Group_1.jpg"
+                        alt="image"> <span class="cs-services__alt">
+                        Ảnh tại các xưởng sản xuất đối tác
+                    </span></div>
+            </div>
+        </div>
+        <div id="services4" class="grid grid--aligned-center grid--two-columns grid--doubling">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #4. Với Môi Trường
+                </h3>
+                <div class="cs-services__description">
+                    <p>Dệt may là ngành gây ra ô nhiễm môi trường lớn thứ 2 trên thế giới chỉ sau ngành giấy. Smart Store
+                        đang cố gắng chuyển mình theo hướng “Substainability” - Thời trang bền vững hơn ở hiện tại và
+                        trong tương lai. Bằng chứng là dòng sản phẩm Excool với sợi Sorona thân thiện với môi trường hơn
+                        nhiều so với sợi Polyester thường. Dự án kết hợp với Cleandye, công nghệ nhuộm sạch, không nước
+                        cũng tạo ra các sản phẩm thân thiện hơn với môi trường. Với dòng các sản phẩm thể thao Smart Store
+                        hướng tới việc ứng dụng trên 50% sợi tái chế (Recycled Polyester) trong các dòng sản phẩm của
+                        mình trong năm 2022 và hướng tới 99% trong năm 2023 trở đi.</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img
+                        src="https://mcdn.coolmate.me/uploads/April2022/Anh_chup_Man_hinh_2021-07-21_luc_16.png"
+                        alt="image"> <span class="cs-services__alt">
+                        Ảnh một công đoạn trong quá trình nhuộm sạch CleanDye
+                    </span></div>
+            </div>
+        </div>
+        <div id="services5" class="grid grid--aligned-center grid--two-columns grid--doubling">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #5. Với Các Cổ Đông
+                </h3>
+                <div class="cs-services__description">
+                    <p>Việc duy trì một mô hình kinh doanh bài bản, có lợi nhuận ổn định và tăng trưởng như là một cam
+                        kết của Coolmate cho cổ đông về việc có được những lợi nhuận và sự tăng giá trị trong dài hạn.
+                    </p>
+                    <p>Đội ngũ Smart Store có rất nhiều những dự định và những ước mơ lớn, tuy nhiên thì chúng tôi bắt đầu
+                        từ những thứ nhỏ nhất, và bước từng bước trên chặng đường đi của mình, chăm chỉ và cố gắng làm
+                        tốt từng ngày. Smart Store không coi bất cứ ai là đối thủ cạnh tranh ngoài chính bản thân mình.
+                        “Tốt hơn mỗi ngày - Better Everyday” là thứ mà chúng tôi theo đuổi.</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img
+                        src="https://mcdn.coolmate.me/uploads/April2022/93865627_10217886564377529_1941617773784334336_n_1.jpg"
+                        alt="image"> <span class="cs-services__alt">
+                        Ảnh đội ngũ Smart Store và Nhà đầu tư 500 Startup
+                    </span></div>
+            </div>
+        </div>
+        <div id="services6" class="grid grid--aligned-center grid--two-columns grid--doubling grid--mobile-rev">
+            <div class="grid__column">
+                <h3 class="cs-services__title">
+                    #6. Với Cộng Đồng Xã Hội
+                </h3>
+                <div class="cs-services__description">
+                    <p>Coolmate cố gắng mở rộng, tạo nhiều việc làm và đóng thuế đầy đủ như một phần đóng góp cho sự
+                        phát triển chung của xã hội.</p>
+                    <p>Quan điểm của Coolmate về việc làm các hoạt động đóng góp cho cộng đồng: </p>
+                    <p>“Một doanh nghiệp không cần phải lớn, mới có thể làm điều ý nghĩa”. Chính vì vậy Coolmate đã mở
+                        ra dự án Care &amp; Share với mục tiêu góp phần hỗ trợ cho các trẻ em khó khăn ở Việt Nam ngay
+                        từ khi mới thành lập công ty được 1 năm. (<a
+                            href="#"
+                            style="text-decoration: underline;">tham khảo thêm dự án Care &amp; Share</a>). Chắc chắn
+                        trong thời gian tới sẽ được đẩy mạnh hơn và hỗ trợ được nhiều hơn.</p>
+                </div>
+            </div>
+            <div class="grid__column">
+                <div class="cs-services__image"><img
+                        src="https://mcdn.coolmate.me/uploads/April2022/Thiet_ke_chua_co_ten_(2)_1.jpg" alt="image">
+                    <span class="cs-services__alt">
+                        Ảnh dự án Áo xây trường được nhiều khách hàng quan tâm &amp; lan tỏa
+                    </span></div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cs-thanks">
+    <div class="container container--medium">
+        <h2 class="cs-thanks__heading">
+            Trong cuộc sống có quá nhiều sự lựa chọn,<br>
+            cảm ơn bạn đã lựa chọn -Smart Store!
+        </h2> <span>“</span>
+    </div>
+</section>
